0ca3a9a9bd2bc30cdcccc86b9c1878e53dd75fdb
CAM603 <cameronhawley@comcast.net>
2019-12-05 09:22:32 -0500

    created cardmaker elements

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,13 +1,19 @@
 /* Step 1: using axios, send a GET request to the following URL 
-           (replacing the palceholder with your Github name):
-           https://api.github.com/users/<your name>
+          (replacing the palceholder with your Github name):
+          https://api.github.com/users/<your name>
 */
-
+axios.get('https://api.github.com/users/CAM603')
+.then( response => {
+  console.log(response)
+})
+.catch( err => {
+  console.log(err);
+})
 /* Step 2: Inspect and study the data coming back, this is YOUR 
-   github info! You will need to understand the structure of this 
-   data in order to use it to build your component function 
+  github info! You will need to understand the structure of this 
+  data in order to use it to build your component function 
 
-   Skip to Step 3.
+  Skip to Step 3.
 */
 
 /* Step 4: Pass the data received from Github into your function, 
@@ -25,6 +31,36 @@
 */
 
 const followersArray = [];
+
+function cardMaker(obj) {
+  // Create elements
+  let card = document.createElement('div');
+  let image = document.createElement('img');
+  let info = document.createElement('div');
+  let name = document.createElement('h3');
+  let username = document.createElement('p');
+  let location = document.createElement('p');
+  let profile = document.createElement('p');
+  let link = document.createElement('a');
+  let followers = document.createElement('p');
+  let following = document.createElement('p');
+  let bio = document.createElement('p');
+
+  // Attach elements to main div
+  card.appendChild(image);
+  card.appendChild(info);
+  info.appendChild(name);
+  info.appendChild(username);
+  info.appendChild(location);
+  info.appendChild(profile);
+  profile.appendChild(link);
+  info.appendChild(followers);
+  info.appendChild(following);
+  info.appendChild(bio);
+
+  console.log(card);
+}
+cardMaker();
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
